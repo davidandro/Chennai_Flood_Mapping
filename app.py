76a7aff3a8ce961b1a5fc6539060f07956f12ced
davidandro <david271306@gmail.com>
2026-05-17 18:24:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import shap
